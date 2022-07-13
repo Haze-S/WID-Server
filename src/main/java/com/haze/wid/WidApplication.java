79b7e938e35e06dcf974f556d156e02569c50023
haze <skdml20022@gmail.com>
2022-07-13 16:58:05 +0900

    프로젝트 세팅

---
+++ src/main/java/com/haze/wid/WidApplication.java
@@ -0,0 +1,13 @@
+package com.haze.wid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haze/wid/WidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haze.wid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
